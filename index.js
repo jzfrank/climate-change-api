da31bec816c9e2829c19dff918123ab1642ec9fa
jzfrank <frankjinzhang@gmail.com>
2022-07-01 10:13:26 +0200

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const PORT = 8000;
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json("Welcome to my climate change news API");
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on PORT ${PORT}`);
+});
