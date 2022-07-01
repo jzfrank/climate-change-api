c6cce2b4ee444e97dcf687325bf6703942378430
jzfrank <frankjinzhang@gmail.com>
2022-07-01 10:48:16 +0200

    scraped from guardian

--- index.js
+++ index.js
@@ -5,8 +5,31 @@ const cheerio = require("cheerio");
 
 const app = express();
 
+const articiles = [];
+
 app.get("/", (req, res) => {
   res.json("Welcome to my climate change news API");
+});
+
+app.get("/news", (req, res) => {
+  axios
+    .get("https://www.theguardian.com/environment/climate-crisis")
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      $('a:contains("climate")', html).each(function () {
+        const title = $(this).text();
+        const url = $(this).attr("href");
+        articiles.push({
+          title,
+          url,
+        });
+      });
+      res.json(articiles);
+
+      console.log(html);
+    })
+    .catch((err) => console.log(err));
 });
 
 app.listen(PORT, () => {
